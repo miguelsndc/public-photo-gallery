0f8ae232ba70b617fe0eacb1004b6c7371040d14
miguel nog <mglsndc@gmail.com>
2021-02-28 17:11:29 -0300

    add dark theme toggle button

--- src/components/Title.js
+++ src/components/Title.js
@@ -6,9 +6,11 @@ const TitleComp = ({ isDarkThemeEnabled, setIsDarkThemeEnabled }) => {
     <StyledTitle>
       <div>
         <h1>Photo Gallery</h1>
-        <button onClick={() => setIsDarkThemeEnabled(!isDarkThemeEnabled)}>
-          dark theme
-        </button>
+        <input
+          type="checkbox"
+          class="toggle"
+          onChange={() => setIsDarkThemeEnabled(!isDarkThemeEnabled)}
+        ></input>
       </div>
       <h2>Public Photo Gallery</h2>
       <p>Share your jobs, your feelings and your art.</p>
