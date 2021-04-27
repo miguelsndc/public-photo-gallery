ac49b91551d1e68660002761a5050f4ab274d496
Miguel Nogueira <mglsndc@gmail.com>
2021-04-27 11:45:41 -0300

    change filename to errorMessages

--- src/error/errorMessages.js
+++ src/error/errorMessages.js
@@ -1,5 +1,5 @@
-const errorTypes = {
+const errorMessages = {
   INVALID_IMAGE_TYPE: 'Please select an valid file type ( PNG / JPEG )',
 };
 
-export default errorTypes;
+export default errorMessages;
