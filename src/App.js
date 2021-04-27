e3943d4c6210a9374bcd3d24563d4d198bdb2015
Miguel Nogueira <mglsndc@gmail.com>
2021-04-27 11:48:21 -0300

    update imports

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import Title from './components/Title';
 import UploadForm from './components/UploadForm';
 import ImageGrid from './components/ImageGrid';
 import Modal from './components/Modal';
+import GlobalStyle from './styles/globals';
+import lightTheme from './styles/themes/light';
+import darkTheme from './styles/themes/dark';
 
 import { ThemeProvider } from 'styled-components';
-
-import GlobalStyle from './styles/globals.sc';
-import { Container } from './styles/App.sc';
-import { lightTheme } from './styles/LightTheme.sc';
-import { darkTheme } from './styles/DarkTheme.sc';
+import { AppContainer } from './styles/AppStyles';
 
 function App() {
   const [selectedImg, setSelectedImg] = useState(null);
   const [isDarkThemeEnabled, setIsDarkThemeEnabled] = useState(false);
 
   return (
-    <Container>
+    <AppContainer>
       <ThemeProvider theme={isDarkThemeEnabled ? darkTheme : lightTheme}>
         <GlobalStyle />
         <Title
@@ -30,7 +29,7 @@ function App() {
           <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
         )}
       </ThemeProvider>
-    </Container>
+    </AppContainer>
   );
 }
 
